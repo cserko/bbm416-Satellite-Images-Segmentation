e30730ae4a8d2b19068784f9c7e9d61648f8a125
root <root@DESKTOP-1VMHO05.localdomain>
2020-07-04 09:48:55 +0300

    coco 1

--- coco_utils.py
+++ coco_utils.py
@@ -51,22 +51,22 @@ class ConvertCocoPolysToMask(object):
     def __call__(self, image, target):
         w, h = image.size
 
-        image_id = target["image_id"]
-        image_id = torch.tensor([image_id])
+        #image_id = target["image_id"]
+        #image_id = torch.tensor([image_id])
 
-        anno = target["annotations"]
+        #anno = target["annotations"]
 
-        anno = [obj for obj in anno if obj['iscrowd'] == 0]
+        #anno = [obj for obj in anno if obj['iscrowd'] == 0]
 
-        boxes = [obj["bbox"] for obj in anno]
+        #boxes = [obj["bbox"] for obj in anno]
         # guard against no boxes via resizing
-        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
-        boxes[:, 2:] += boxes[:, :2]
-        boxes[:, 0::2].clamp_(min=0, max=w)
-        boxes[:, 1::2].clamp_(min=0, max=h)
+        #boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
+        #boxes[:, 2:] += boxes[:, :2]
+        #boxes[:, 0::2].clamp_(min=0, max=w)
+        #boxes[:, 1::2].clamp_(min=0, max=h)
 
-        classes = [obj["category_id"] for obj in anno]
-        classes = torch.tensor(classes, dtype=torch.int64)
+        #classes = [obj["category_id"] for obj in anno]
+        #classes = torch.tensor(classes, dtype=torch.int64)
 
         segmentations = [obj["segmentation"] for obj in anno]
         masks = convert_coco_poly_to_mask(segmentations, h, w)
